c69d64545adfa984837b0636f5445fe09152c400
Yana Ushakova <junk7day@gmail.com>
2023-02-22 13:33:49 +0300

    creation and registration receiver

--- app/src/main/java/com/example/broadcastreceiverstest/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverstest/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.broadcastreceiverstest
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(receiver, intentFilter)
     }
 }
---
+++ app/src/main/java/com/example/broadcastreceiverstest/MyReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.broadcastreceiverstest
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action) {
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                Toast.makeText(context, "Airplane mode", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
